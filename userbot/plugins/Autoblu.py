061f805d6d23fb400e23147d120265c28109d8fb
ohskiller <69169906+ohskiller@users.noreply.github.com>
2020-08-25 13:00:09 +0200

    Create Autoblu.py

---
+++ userbot/plugins/Autoblu.py
@@ -0,0 +1,40 @@
+#By @CometaLunare on TG, all rights deserved
+
+import asyncio
+from telethon import events
+from userbot import bot
+from userbot.system import dev_cmd
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.2
+
+    animation_ttl = range(0, 117)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "autoblu2":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+            "By @OHS_Kill3r05",
+            "AUTO BLU🔴🔵",
+            "AUTO BLU🔵🔴",
+			 "AUTO BLU🔴🔵",
+			 "AUTO BLU🔵🔴",
+			 "Bimbo catturato!",
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 117])
