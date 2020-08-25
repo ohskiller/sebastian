84c285bf945b7e1e770ef55def5ccfd40f6a81b8
ohskiller <69169906+ohskiller@users.noreply.github.com>
2020-08-25 12:58:38 +0200

    Create Gay.py

---
+++ userbot/plugins/Gay.py
@@ -0,0 +1,48 @@
+#By @OHS_Kill3r05 on TG, all rights deserved
+
+import asyncio
+from telethon import events
+from userbot import bot
+from userbot.system import dev_cmd
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.4
+
+    animation_ttl = range(0, 117)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "gay":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+            "By @OHS_Kill3r05",
+            "caricamento...",
+            "caricamento..",
+            "caricamento.",
+            "caricamento...",
+            "caricamento..",
+            "caricamento.",
+            "caricamento...",
+            "caricamento..",
+            "caricamento.",
+            "caricamento...",
+            "caricamento..",
+            "caricamento.",
+            "sei appena diventato GAY🏳‍🌈",
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 117])
