f888f70e4f1381a9976e88c2105ef933bea8ff05
ohskiller <69169906+ohskiller@users.noreply.github.com>
2020-10-15 10:44:48 +0200

    Update spam.py

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -22,20 +22,3 @@ async def spammer(e):
             )
 
 
-@register(outgoing=True, pattern="^.bigspam")
-async def bigspam(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        message = e.text
-        counter = int(message[9:13])
-        spam_message = str(e.text[13:])
-        for i in range(1, counter):
-            await e.respond(spam_message)
-        await e.delete()
-        if BOTLOG:
-            await e.client.send_message(
-                BOTLOG_CHATID,
-                "#BIGSPAM\n"
-                "Bigspam eseguita"
-                )
-
-
